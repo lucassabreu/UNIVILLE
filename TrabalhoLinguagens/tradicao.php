5f36f7c940a139c1243fefb58c5484d8d3a98b8f
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2013-03-24 17:52:07 -0300

    tradicao.php

--- TrabalhoLinguagens/tradicao.php
+++ TrabalhoLinguagens/tradicao.php
@@ -7,4 +7,15 @@ printf("%s tem %d anos\n\n", $nome, $idade);
 
 // comando echo
 echo "$nome tem $idade anos\n\n";
+
+$_GET['total'] = 90;
+
+$total = 0;
+voltar:
+    $total = $_GET['total'] + $total;
+    
+    if ($total < 100)
+        goto voltar;
+    else
+        echo $total . "\n";
 ?>
