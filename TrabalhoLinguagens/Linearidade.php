b551560e49b14869610ab12bca5ef06eff4eface
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2013-03-24 19:18:47 -0300

    Linearidade

---
+++ TrabalhoLinguagens/Linearidade.php
@@ -0,0 +1,13 @@
+<?php
+
+$arr1 = array(array('usuario' => 'usuario01'), array('usuario' => 'usuario02'));
+
+$arr2 = array(
+    array(
+        'usuario' => 'usuario01'
+    ),
+    array(
+        'usuario' => 'usuario02'
+    ),
+);
+?>
