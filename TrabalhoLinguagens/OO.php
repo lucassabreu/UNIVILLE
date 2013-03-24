215556da1975e0930fd8dd79009b69edc830bf18
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2013-03-24 16:37:43 -0300

    Simplificando

--- TrabalhoLinguagens/OO.php
+++ TrabalhoLinguagens/OO.php
@@ -1,29 +1,18 @@
 <?php
-
 class Pessoa {
-
     public $nome;
-    public $cpf;
-    public $rg;
     public $dataNascimento;
 
-    function __construct($nome, $cpf, $rg, \DateTime $dataNascimento) {
+    function __construct($nome, \DateTime $dataNascimento) {
         $this->nome = $nome;
-        $this->cpf = $cpf;
-        $this->rg = $rg;
         $this->dataNascimento = $dataNascimento;
     }
 
     public function idade() {
-        if ($this->dataNascimento === null)
-            return 0;
-        else
-            return intval($this->dataNascimento->diff(new \DateTime)->format('%y'));
+        return intval($this->dataNascimento->diff(new \DateTime)->format('%y'));
     }
-
 }
 
 $pessoa = new Pessoa('Joao', '085489655478', '5612448', new \DateTime('01/01/1994'));
-
 echo "Idade de $pessoa->nome eh " . $pessoa->idade() . "\n";
 ?>
