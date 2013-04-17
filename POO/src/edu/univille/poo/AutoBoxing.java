b42d058c441aac13f91fcd8e53726d990552dedc
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2013-04-17 15:22:45 -0300

    ALL

---
+++ POO/src/edu/univille/poo/AutoBoxing.java
@@ -0,0 +1,20 @@
+package edu.univille.poo;
+
+public class AutoBoxing {
+	public static void main(String[] args) {
+		int idade = 20;
+		Integer minhaIdade = 20;
+		Integer minhaIdade2 = new Integer(20);
+		
+		long timeIni = System.currentTimeMillis();
+
+		for(Integer i = 0; i < 99999999; ) {
+			i = i + 1;
+		}
+		
+		long timeFim = System.currentTimeMillis();
+
+		long diff = timeFim - timeIni;
+		System.out.println("Tempo: " + diff);
+	}
+}
