c5e676a6887c0e18e407ef97c7062d1952cd7206
Lucas dos Santos Abreu <lucas.s.abreu@gmail.com>
2014-04-03 17:59:24 -0300

    Pilhas

---
+++ 3ANO/EstruturaDados/src/br/univille/aula5/pilha/EmptyStackException.java
@@ -0,0 +1,9 @@
+package br.univille.aula5.pilha;
+
+public class EmptyStackException extends RuntimeException {
+    private static final long serialVersionUID = 5676755395834842375L;
+
+    public EmptyStackException(String mes) {
+        super(mes);
+    }
+}
